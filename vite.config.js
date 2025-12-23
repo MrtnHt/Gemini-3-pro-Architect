534167dd6be0e5c5eed4e41f4e82f4426557f1d7
MrtnHt <144637962+MrtnHt@users.noreply.github.com>
2025-12-23 01:39:45 +0100

    Set base path for GitHub Pages
    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Dit zorgt dat de app snapt dat hij op GitHub Pages staat
 export default defineConfig({
   plugins: [react()],
   base: '/Gemini-3-pro-Architect/',
